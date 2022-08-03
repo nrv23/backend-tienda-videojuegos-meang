621c9ec7f3dc74212489de80686561d2d5650dcc
nrv23 <navemen23@hotmail.com>
2022-08-02 18:27:49 -0600

    cambiar respuesta de la mutacion de registro, agregando un type ResultRegister

--- src/resolvers/mutation/user.ts
+++ src/resolvers/mutation/user.ts
@@ -21,19 +21,34 @@ const mutationResolvers: IResolvers = {
           );
 
           if (!registerResponse) {
-            return "No se pudo agregar el usuario";
+          
+            return {
+              status: false,
+              message: "No se pudo agregar el usuario"
+            }
           }
 
           else if(registerResponse === "existe") {
-            return "No puede registrar un usuario que ya existe anteriormente";
+            return {
+              status: false,
+              message: "No puede registrar un usuario que ya existe anteriormente"
+            }
+
           }
 
-          return "Usuario agregado con éxito";
+          return  {
+            status: true,
+            message: "Usuario registrado con éxito"
+          }
+;
 
         } catch (error) {
           console.log({ error });
 
-          return "Error al agregar el usuario";
+          return {
+            status: false,
+            message: "Error al registrar el usuario"
+          };
         }
       } catch (error) {
         console.log({ error });
