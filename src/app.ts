757584989082fa6d08d8633715b89c7214b92954
nrv23 <navemen23@hotmail.com>
2022-08-02 19:16:24 -0600

    backup de base de datos

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,13 @@ import schema from "./schema";
 const graphQLServer = new GraphQLServer(schema);
 
 graphQLServer.listen((port: number) => console.log(`http://localhost:${port}/graphql`));
+
+/*
+    Comando para restaurar bases de datos mongo db
+    mongorestore -d tienda db/rawg/
+
+    -d para indicar la base de datos
+    luego la ubicacion de lo archivos json y bson
+    luego darle enter
+
+*/
